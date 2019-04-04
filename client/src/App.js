1c68eef9b0453ace26209396422518f9862e07d4
Nick Meisburger <ncm5@rice.edu>
2019-04-04 15:32:45 -0500

    components

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,22 @@
 import React, { Component } from 'react';
-import MBar from './displays/components/MBar';
-
-import './componentStyles/styles.css';
-import './componentStyles/inputStyles.css';
+import Navbar from './displays/components/navbar/navbar';
+import McTeam from './displays/components/mcteam/mcteam';
+import McMinistry from './displays/components/mcministry/mcministry';
+import Court from './displays/components/court/court';
+import Committees from './displays/components/committees/committees';
+import DivisionalAdvisors from './displays/components/divisionaladvisors/divisionaladvisors';
+import './componentStyles/styles.css'
 
 class App extends Component {
   render() {
     return (
       <div>
-        <MBar default />
+        <Navbar />
+        {/* <McTeam /> */}
+        {/* <McMinistry /> */}
+        {/* <Court /> */}
+        {/* <Committees /> */}
+        {/* <DivisionalAdvisors /> */}
         {/* Please define all actual displays in the displays folder */}     
       </div>
     );
