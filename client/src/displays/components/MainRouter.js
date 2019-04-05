7e405144300b1fa2208c816e94b957e3e806bae0
Nick Meisburger <ncm5@rice.edu>
2019-04-05 18:14:37 -0500

    reconcile changes

--- client/src/displays/components/MainRouter.js
+++ client/src/displays/components/MainRouter.js
@@ -9,11 +9,13 @@ import McMinistry from './navbar headers/residential leadership/mcministry/mcmin
 import Court from './navbar headers/residential leadership/court/court';
 import Committees from './navbar headers/residential leadership/committees/committees';
 import DivisionalAdvisors from './navbar headers/academics/divisionaladvisors/divisionaladvisors';
-<<<<<<< HEAD
 import PAAs from './navbar headers/academics/paas/paas';
 import AcademicFellows from './navbar headers/academics/fellows/fellows';
 import ResidentHealthAdvisors from './navbar headers/wellbeing/resident_health_advisors';
 import StriveLiaisons from './navbar headers/wellbeing/strive_liaisons';
+import WorkOrder from './navbar headers/resources/workorder/workorder';
+import InitiativeRequest from './navbar headers/resources/initiativerequest/initiativerequest';
+import EquipmentRequest from './navbar headers/resources/equipmentrequest/equipmentrequest';
 
 class MainRouter extends Component {
   render() {
@@ -35,42 +37,14 @@ class MainRouter extends Component {
               <Route exact path="/academicfellows" component={AcademicFellows} />
               <Route exact path='/RHAs' component={ResidentHealthAdvisors} />
               <Route exact path="/striveliaisons" component={StriveLiaisons} />
+              <Route exact path="/workorder" component={WorkOrder} />
+              <Route exact path="/initiativerequest" component={InitiativeRequest} />
+              <Route exact path="/equipment" component={EquipmentRequest} />
             </Switch>
           </div>
         </Router>
       </div>
     );
-=======
-import WorkOrder from './navbar headers/resources/workorder/workorder';
-import InitiativeRequest from './navbar headers/resources/initiativerequest/initiativerequest';
-import EquipmentRequest from './navbar headers/resources/equipmentrequest/equipmentrequest';
-
-class MainRouter extends Component {
-    render() {
-      return (
-        <div>
-          <Router>
-            <div>
-              <Navbar />
-              <Switch>
-                <Route exact path="/about" component={About}/>
-                <Route exact path="/events" component={Events}/>
-                <Route exact path="/artevents" component={ArtEvents}/>
-                <Route exact path="/mcteam" component={McTeam}/>
-                <Route exact path="/mcministry" component={McMinistry}/>
-                <Route exact path="/court" component={Court}/>
-                <Route exact path="/committees" component={Committees}/>
-                <Route exact path="/da" component={DivisionalAdvisors}/>
-                <Route exact path="/workorder" component={WorkOrder}/>
-                <Route exact path="/initiativerequest" component={InitiativeRequest}/>
-                <Route exact path="/equipment" component={EquipmentRequest}/>
-              </Switch>
-            </div>
-          </Router>
-        </div>
-      );
-    }
->>>>>>> 114c304a5b9fd0dd7bc3c96ba80eb8be2f7f892d
   }
 }
 
