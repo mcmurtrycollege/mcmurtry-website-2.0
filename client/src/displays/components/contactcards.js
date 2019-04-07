b23b617e171bcb3906393d0a7a8e237b24eaf51e
Nick Meisburger <ncm5@rice.edu>
2019-04-06 23:47:45 -0500

    spashpage update

--- client/src/displays/components/contactcards.js
+++ client/src/displays/components/contactcards.js
@@ -3,36 +3,44 @@ import { Box, Flex } from 'rebass';
 import './contactcards.css';
 
 const Card = props => (
-    props.position !== undefined ? (
-        <Box className='position-card' width={260}>
-            <div className="position-card-content">
-                <h2 className="position">{props.position}</h2>
-                <h2 className="name">{props.name}</h2>
-                <a href={`mailto:${props.email}`} className='contact'>
-                    <h3 className="contact-email"><img src="https://icon.now.sh/arrow/422a4C/10" />{' '}{props.email}</h3>
-                </a>
-                <h4 className='year'>Year: {props.year}</h4>
-            </div>
-        </Box>
-    ) : (
-        <Box className='position-card' width={260}>
-            <div className="positionless-card-content">
-                <h2 className="name">{props.name}</h2>
-                <a href={`mailto:${props.email}`} className='contact'>
-                    <h3 className="contact-email"><img src="https://icon.now.sh/arrow/422a4C/10" />{' '}{props.email}</h3>
-                </a>
-                <h4 className='year'>Year: {props.year}</h4>
-            </div>
-        </Box>
-    )
+
+    < Box className='position-card' width={260} >
+        <div className="position-card-content">
+            {
+                props.position !== undefined ? (
+                    <h2 className="position">{props.position}</h2>
+                ) : (
+                        null
+                    )
+            }
+            <h2 className="name">{props.name}</h2>
+            {
+                props.phone !== undefined ? (
+                    <h3 className="contact-phone"><img src="https://icon.now.sh/call/422a4C/16" />{' '}{props.phone}</h3>
+                ) : (
+                    null
+                )
+            }
+            <a href={`mailto:${props.email}`} className='contact'>
+                <h3 className="contact-email"><img src="https://icon.now.sh/arrow/422a4C/10" />{' '}{props.email}</h3>
+            </a>
+            {
+                props.year !== undefined ? (
+                    <h4 className='year'>Year: {props.year}</h4>
+                ) : (
+                    null
+                )
+            }
+        </div>
+    </Box >
 )
 
 const Cards = props => (
     <Box width={[1, 1, 0.9, 0.8]} ml='auto' mr='auto'>
         <Flex flexWrap='wrap' flexDirection='row' justifyContent='center'>
             {
-                props.content.map(({ position, name, year, email }) => (
-                    <Card key={name} position={position} name={name} year={year} email={email} />
+                props.content.map(({ position, name, year, email, phone }) => (
+                    <Card key={name} position={position} name={name} year={year} email={email} phone={phone} />
                 ))
             }
         </Flex>
--- client/src/displays/components/spashpage/intropage.js
+++ client/src/displays/components/spashpage/intropage.js
@@ -3,14 +3,14 @@ import { Box, Image } from 'rebass';
 import Title from '../title';
 import './intropage.css';
 
-import Yurt from '../../static/YurtTest.svg';
+import Yurt from '../../static/YurtTrees.svg';
 
 const Intro = () => (
     <div className='intro-page'>
 
         {/* Note: This is a temporary image to act as a placeholder */}
 
-        <Box width={0.6} ml='auto' mr='auto'>
+        <Box width={0.8} ml='auto' mr='auto'>
             <Image src={Yurt} />
         </Box>
     </div>
