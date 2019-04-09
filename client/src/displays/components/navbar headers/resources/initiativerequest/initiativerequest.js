d642be8fe96c00c091ce951f220ba62449ea2e8e
Shaquille Wyan Que <shaquillewyanque@shaquilles-mbp.dyndns.rice.edu>
2019-04-09 13:35:01 -0500

    fixed responsiveness

--- client/src/displays/components/navbar headers/resources/initiativerequest/initiativerequest.js
+++ client/src/displays/components/navbar headers/resources/initiativerequest/initiativerequest.js
@@ -6,8 +6,11 @@ import './initiativerequest.css';
 const InitiativeRequest = () => (
     <div className='initiative-request-page'>
         <Title width={230} title="Initiative Request" />
-        <div className='initiative-request-form'>
+        <div className='wide-initiative-request-form'>
             <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSfWw9BThDp7wDgxtYYvwRomD-myrMXZYP6ve-TzincU81ViBg/viewform?embedded=true" width="700" height="2000" frameBorder="0" scrolling="yes" type="text/javascript" title="Initiatives Request"></iframe>
+        </div>
+        <div className='narrow-initiative-request-form'>
+            <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSfWw9BThDp7wDgxtYYvwRomD-myrMXZYP6ve-TzincU81ViBg/viewform?embedded=true" width="360" height="2000" frameBorder="0" scrolling="yes" type="text/javascript" title="Initiatives Request"></iframe>
         </div>
     </div>
 )
