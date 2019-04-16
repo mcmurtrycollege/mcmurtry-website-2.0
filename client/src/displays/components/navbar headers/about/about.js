8d68c8479fdaf740984350033fffff2f71c66f22
Nick Meisburger <ncm5@rice.edu>
2019-04-16 18:24:44 -0500

    minor adjustments

--- client/src/displays/components/navbar headers/about/about.js
+++ client/src/displays/components/navbar headers/about/about.js
@@ -9,7 +9,7 @@ const About = () => (
     <div className='about-page'>
         <Box className='about-text' width={[1, 0.9, 0.8, 0.7]} ml='auto' mr='auto' mb={50}>
             <Box className='about-subtitle' mr='auto' ml='auto' width={290} my={20}>
-                <h1 style={{margin: '8px'}}>About McMurtry</h1>
+                <h1 style={{margin: '8px', fontWeight: 'normal'}}>About McMurtry</h1>
             </Box>
             <p>
                 <Image src={mcm_commons} alt='' width={360} style={{ padding: '10px 20px', float: 'left' }} />
@@ -18,7 +18,7 @@ const About = () => (
         </Box>
         <Box className='about-text' width={[1, 0.9, 0.8, 0.7]} ml='auto' mr='auto' mb={50}>
             <Box className='about-subtitle' mr='auto' ml='auto' width={[300, 480]} my={20}>
-                <h1 style={{margin: '8px'}}>Burt and Deedee McMurtry</h1>
+                <h1 style={{margin: '8px', fontWeight: 'normal'}}>Burt and Deedee McMurtry</h1>
             </Box>
             <p>
                 <Image src={burt_deedee} alt='' width={320} style={{ padding: '10px 20px', float: 'right' }} />
